c9454b76412848e04ecdd079fa1878e804d81397
Dimitri Goldshtein <israodin@gmail.com>
2020-06-30 00:50:29 +0300

    add kubernetes-operators build and templates

--- kubernetes-operators/build/mysql-operator.py
+++ kubernetes-operators/build/mysql-operator.py
@@ -3,3 +3,20 @@ import yaml
 import kubernetes
 import time
 from jinja2 import Environment, FileSystemLoader
+
+def render_template(filename, vars_dict):
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template(filename)
+    yaml_manifest = template.render(vars_dict)
+    json_manifest = yaml.load(yaml_manifest)
+    return json_manifest
+
+@kopf.on.create('otus.homework', 'v1', 'mysqls')
+# Функция, которая будет запускаться при создании объектов тип MySQL:
+def mysql_on_create(body, spec, **kwargs):
+    name = body['metadata']['name']
+    image = body['spec']['image'] # cохраняем в переменные содержимое описания
+    MySQL из CR
+    password = body['spec']['password']
+    database = body['spec']['database']
+    storage_size = body['spec']['storage_size']
